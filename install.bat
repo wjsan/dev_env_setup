6504486faab0988d9c2ec926732b18ca77b8afa8
Wesley Jose Santos <binary_quantum@outlook.com>
2023-08-03 11:53:36 -0300

    feat: refresh environment variables before install python

--- install.bat
+++ install.bat
@@ -3,6 +3,9 @@
 echo Installing Chocolatey...
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
+echo Refreshing Environment Variables...
+refreshenv
+
 echo Installing Python...
 choco install python -y
 
