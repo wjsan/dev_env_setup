aa11b9b970178a5207f3d60e8988d9bf7adbfa98
Wesley Jose Santos <binary_quantum@outlook.com>
2023-08-03 13:29:17 -0300

    fix: fix call

--- install.bat
+++ install.bat
@@ -4,14 +4,14 @@ echo Installing Chocolatey...
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
 echo Refreshing Environment Variables...
-goto refreshenv
+call :refreshenv
 
 echo Installing Python...
 choco install python -y
 echo Python Installed!
 
 echo Refreshing Environment Variables...
-goto refreshenv
+call :refreshenv
 
 echo Installing Dev Tools...
 python install.py
