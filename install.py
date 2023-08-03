ad0d2d8d7921347a95627d69cee8148b8cc7dc1b
Wesley Jose Santos <binary_quantum@outlook.com>
2023-08-03 17:56:20 -0300

    feat: update script

--- install.py
+++ install.py
@@ -7,20 +7,19 @@ def install_tool(tool):
     try:
         subprocess.run(tool['cmd'], shell=True, check=True)
         print(f"{tool['name']} installed successfully.")
-        if tool.get("sleep"):
-            time.sleep(tool.get("sleep"))
-        if tool.get("refreshEnv"):
-            subprocess.run("refreshenv", shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Failed to install {tool['name']}. Error: {e}")
 
 def main():
     with open('tools.json', 'rb') as file:
         content = file.read()
-    
     tools = json.loads(content)["tools"]
     for tool in tools:
-        install_tool(tool)
+        if(tool.get("enabled")):
+            install_tool(tool)
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    elapsed_time = time.time() - start_time
+    print(f"Installation process completed in {elapsed_time:.2f} seconds.")
